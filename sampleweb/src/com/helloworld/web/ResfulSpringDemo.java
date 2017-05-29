a809f9c29920ae24af5bfdd6467b1d49a32ab140
santosh <santosh.ece06@gmail.com>
2017-05-29 23:41:52 +0530

    commit.

--- sampleweb/src/com/helloworld/web/ResfulSpringDemo.java
+++ sampleweb/src/com/helloworld/web/ResfulSpringDemo.java
@@ -1,11 +1,19 @@
 package com.helloworld.web;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.util.component.Post;
 
 @Controller
 @RequestMapping("/ctrl1")
@@ -24,5 +32,10 @@ public class ResfulSpringDemo {
 	}
 	
 	
-	
+	@PostMapping("/hello5")
+	public ResponseEntity<List<Post>> sayHello5(@RequestBody Post post) {
+		
+		List<Post> postList = Collections.singletonList(post);
+		return new ResponseEntity<>(postList, HttpStatus.OK);
+	}
 }
--- sampleweb/src/com/helloworld/web/RestfuleSpringDemo1.java
+++ sampleweb/src/com/helloworld/web/RestfuleSpringDemo1.java
@@ -1,12 +1,18 @@
 package com.helloworld.web;
 
+import java.util.Collections;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.util.component.Post;
@@ -16,7 +22,8 @@ import com.util.component.Post;
 public class RestfuleSpringDemo1 {
 
 	@GetMapping("/hello1")
-	public String sayHello1(HttpServletRequest request, HttpServletResponse response) {
+	public String sayHello1(HttpServletRequest request, HttpServletResponse response, @RequestParam("name") String name) {
+		String str = request.getParameter("name");
 		return "hello1 at ctrol2";
 	}
 	
@@ -35,6 +42,23 @@ public class RestfuleSpringDemo1 {
 		post.setPost("Hello Post");
 		
 		return new ResponseEntity<Post>(post, HttpStatus.OK);
+	}
+
+	@GetMapping("/hello4")
+	public ResponseEntity<List<Post>> sayHello4() {
+		Post post = new Post();
+		post.setPost("Hello Post");
+		
+		List<Post> postList = Collections.singletonList(post);
+		return new ResponseEntity<>(postList, HttpStatus.OK);
+	}
+	
+	
+	@PostMapping("/hello5")
+	public ResponseEntity<List<Post>> sayHello5(@RequestBody Post post) {
+		
+		List<Post> postList = Collections.singletonList(post);
+		return new ResponseEntity<>(postList, HttpStatus.OK);
 	}
 
 }
